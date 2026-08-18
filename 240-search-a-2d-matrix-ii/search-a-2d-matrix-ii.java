6876c904d57868ef0a75a204b41dd8378d67d854
isaltanmay-hash <isaltanmay@gmail.com>
2026-08-18 11:01:14 +0530

    Time: 3 ms (87.90%) | Memory: 47.6 MB (98.97%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,10 +1,13 @@
-//brute force
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==target)return true;
-            }
+        int i=0;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int j=n-1;
+        while(i<m && j>=0){
+            if(matrix[i][j]>target)j--;
+            else if(matrix[i][j]<target)i++;
+            else return true;
         }
         return false;
     }
