64fd739e90ab18a2e26416d3bcd48d83e47006b2
isaltanmay-hash <isaltanmay@gmail.com>
2026-08-18 09:55:09 +0530

    Time: 2 ms (11.16%) | Memory: 43.6 MB (34.76%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -6,12 +6,12 @@ class Solution {
             for(int j=0;j<=i;j++){
                 if(j==0 || j==i)ans.get(i).add(1);
                 else{
-                    ans.get(i).add(j,ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+                    //ans.get(i).add(j,ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
                     //or
-                    /*
-                    int val=ans.get(i-j).get(j)+ans.get(i-1).get(j-1);
-                    ans.get(i).add(j,val)
-                    */
+                    
+                    int val=ans.get(i-1).get(j)+ans.get(i-1).get(j-1);
+                    ans.get(i).add(j,val);
+                    
                 }
             }
         }
