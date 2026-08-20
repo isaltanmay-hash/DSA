cfab50980c2eb4b6609d958a6f88b45d0d947ea3
isaltanmay-hash <isaltanmay@gmail.com>
2026-08-20 20:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (44.98%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int fr=0;
+        int lr=n-1;
+        int fc=0;
+        int lc=n-1;
+
+        int arr[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=0;
+            }
+        }
+
+        int count=0;
+        while(count<=n*n){
+            //right
+            for(int i=fc;i<=lc;i++){
+                arr[fr][i]=++count;
+            }
+            fr++;
+            if(count==n*n)break;
+
+            //down
+            for(int i=fr;i<=lr;i++){
+                arr[i][lc]=++count;
+            }
+            lc--;
+            if(count==n*n)break;
+
+            //left
+            for(int i=lc;i>=fc;i--){
+                arr[lr][i]=++count;
+            }
+            lr--;
+            if(count==n*n)break;
+
+            //up
+            for(int i=lr;i>=fr;i--){
+                arr[i][fc]=++count;
+            }
+            fc++;
+            if(count==n*n)break;
+        }
+        return arr;
+
+    }
+}
