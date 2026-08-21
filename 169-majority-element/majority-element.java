4990d67e5c348a55f7256dd96f4a43b2a8b9e3ee
isaltanmay-hash <isaltanmay@gmail.com>
2026-08-21 21:15:59 +0530

    Time: 2 ms (70.95%) | Memory: 55.1 MB (70.84%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,14 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int cand=0;
+        int count=0;
         for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j])count++;
+            if(count==0){
+                cand=nums[i];
             }
-            if(count>nums.length/2){
-                return nums[i];
+            if(nums[i]==cand){
+                count++;
+            }
+            else{
+                count--;
             }
         }
-        return -1;
+        return cand;
     }
 }
