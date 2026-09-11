4369a92efd99e68bc80c4c74a0a9b5bc2a0d9982
isaltanmay-hash <isaltanmay@gmail.com>
2026-09-11 13:16:33 +0530

    Time: 582 ms (5.24%) | Memory: 42.3 MB (89.33%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,5 +1,11 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        return n;
+        int sumOdd=n*n;
+        int sumEven=n*(n+1);
+
+        for(int i=Math.min(sumOdd,sumEven);i>0;i--){
+            if(sumOdd%i==0 && sumEven%i==0)return i;
+        }
+        return 1;
     }
 }
