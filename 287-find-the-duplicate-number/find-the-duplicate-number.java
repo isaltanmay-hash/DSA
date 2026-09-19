94e239e8809a072deeb174025ecaeb97ff599fb3
isaltanmay-hash <isaltanmay@gmail.com>
2026-09-19 19:57:03 +0530

    Time: 38 ms (11.43%) | Memory: 79.2 MB (94.73%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,18 +1,9 @@
-//by using cycle sort technique
+//better O(nlogn)
 class Solution {
     public int findDuplicate(int[] nums) {
-        int i=0;
-        while(i<nums.length){
-            if(nums[i]==i+1){
-                i++;
-            }
-            else{
-                int idx=nums[i]-1;
-                int temp=nums[i];
-                nums[i]=nums[idx];
-                nums[idx]=temp;
-                if(nums[i]==nums[idx]) return nums[i];
-            }
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1])return nums[i];
         }
         return -1;
     }
