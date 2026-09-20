cd19bd615e76d4244e65a8fef7f6cf2d7d653cca
isaltanmay-hash <isaltanmay@gmail.com>
2026-09-20 11:34:52 +0530

    Time: 3 ms (99.94%) | Memory: 62.2 MB (45.81%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,35 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
-
+//by freaquency array
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> al = new ArrayList<>();
-        int i = 0;
-        
-        // Step 1: Cyclic Sort Phase
-        while (i < nums.length) {
-            int correctIdx = nums[i] - 1;
-            
-            // Swap only if the current number is not at its correct index
-            // AND it is not a duplicate of the number already at that correct index
-            if (nums[i] != nums[correctIdx]) {
-                int temp = nums[i];
-                nums[i] = nums[correctIdx];
-                nums[correctIdx] = temp;
-            } else {
-                // Move forward if it's already in the right spot OR a duplicate is detected
-                i++;
+        List<Integer> al=new ArrayList<>();
+        int[] freq=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]-1]++;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(freq[i]>1){
+                al.add(i+1);
             }
         }
-        
-        // Step 2: Identification Phase
-        // Any number not matching its index slot is a duplicate
-        for (i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1) {
-                al.add(nums[i]);
-            }
-        }
-        
         return al;
     }
 }
